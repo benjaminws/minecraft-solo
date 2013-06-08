578c8065f5ff10f0fd118e0ff40e7d9aa071e126
Benjamin W. Smith <benjaminwarfield@just-another.net>
2013-06-08 16:58:55 -0400

    Speed up the file sync. Needs some love later though.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
-HOST="$1"
 : ${DEPLOY_USER:=$USER}
 
-echo "Running chef-solo with the contents of this repo on $DEPLOY_USER@$HOST"
-tar cj . | ssh -o 'StrictHostKeyChecking no' "$DEPLOY_USER@$HOST" '
-    sudo rm -rf ~/chef &&
-    mkdir ~/chef &&
-    cd ~/chef &&
-    tar xj &&
-    sudo CHEF_USER=$USER /usr/bin/chef-solo -c solo.rb -j solo.js'
+function tar_sync {
+    # Deprecating this because it's slow. Good times though.
+    local hostname=$1; shift;
+    tar cj . | ssh -o 'StrictHostKeyChecking no' "$DEPLOY_USER@$HOST" '
+        sudo rm -rf ~/chef &&
+        mkdir ~/chef &&
+        cd ~/chef &&
+        tar xj &&
+        sudo CHEF_USER=$USER /usr/bin/chef-solo -c solo.rb -j solo.js'
+}
+
+function rsync_sync {
+    # Make this less greedy. Generate a file list or something
+    local hostname=$1; shift;
+    rsync -av -H --progress --delete . $DEPLOY_USER@$hostname:~/chef
+    run_chef_solo $hostname;
+}
+
+function run_chef_solo {
+    local hostname=$1; shift;
+    ssh -o 'StrictHostKeyChecking no' "$DEPLOY_USER@$hostname" 'cd ~/chef && sudo CHEF_USER=$USER /usr/bin/chef-solo -c solo.rb -j solo.js'
+}
+
+function main {
+    local hostname=$1; shift;
+    echo "Running chef-solo with the contents of this repo on $DEPLOY_USER@$hostname"
+    rsync_sync $hostname;
+}
+
+main $*;
